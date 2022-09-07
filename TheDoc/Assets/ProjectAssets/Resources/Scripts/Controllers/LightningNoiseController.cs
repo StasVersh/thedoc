d09b6d1cfa042b361e052784ab64fdca6328b3f0
StasVersh <71280619+StasVersh@users.noreply.github.com>
2022-09-08 02:12:44 +0300

    Remake LampPost

---
+++ TheDoc/Assets/ProjectAssets/Resources/Scripts/Controllers/LightningNoiseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using Unity.Burst.Intrinsics;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
+
+namespace ProjectAssets.Resources.Scripts.Controllers
+{
+    [RequireComponent(typeof(Light2D))]
+    public class LightningNoiseController : MonoBehaviour
+    {
+        [SerializeField] private float _noise;
+        [SerializeField] private float _minValue;
+        [SerializeField] private float _maxValue;
+        private Light2D _light;
+
+        private void Start()
+        {
+            _light = GetComponent<Light2D>();
+            //StartCoroutine(Noise());
+        }
+
+        private void Update()
+        {
+            _light.intensity += (float)Math.Sin(Time.time) / _noise;
+
+            if (_light.intensity > _maxValue || _light.intensity < _minValue)
+            {
+                _light.intensity = (_maxValue + _minValue) / 2;
+            }
+        }
+
+        private IEnumerator Noise()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(_noise / 1000);
+                _light.intensity = Random.Range(_minValue, _maxValue);
+            }
+        }
+    }
+}
