41c623b0640a04c14164e7f4ac4bf22d8717e0c7
starikandrei <90604262+starikandrei@users.noreply.github.com>
2022-09-09 22:44:16 +0300

    delete dush

--- TheDoc/Assets/ProjectAssets/Resources/Doc/Scripts/States/FallingState.cs
+++ TheDoc/Assets/ProjectAssets/Resources/Doc/Scripts/States/FallingState.cs
@@ -1,6 +1,5 @@
 ﻿using ProjectAssets.Resources.Doc.Scripts.Model;
 using ProjectAssets.Resources.Doc.Scripts.Values;
-using UnityEngine.InputSystem;
 using StateMachine = ProjectAssets.Resources.Doc.Scripts.Controllers.StateMachine;
 
 namespace ProjectAssets.Resources.Doc.Scripts.States
@@ -15,12 +14,6 @@ namespace ProjectAssets.Resources.Doc.Scripts.States
         {
             base.Enter();
             _player.Controller.SetAnimation(PlayerAnimations.Falling);
-            _player.Input.PlayerInput.Jump.started += JumpOnStarted;
-            _player.Input.PlayerInput.Hover.started += HoverOnStarted;
-            if (_player.Input.PlayerInput.Jump.IsPressed() && _player.HaveHover)
-            {
-                _stateMachine.ChangeState(_player.States.HoveringState);
-            }
         }
 
         public override void LogicUpdate()
@@ -28,39 +21,13 @@ namespace ProjectAssets.Resources.Doc.Scripts.States
             base.LogicUpdate();
             if (!_player.CanJump) return;
             _stateMachine.ChangeState(_player.States.GroundedBaseState);
-            _player.FallParticles.Play();
-            if (!_player.IsFalling)
-            {
-                _player.Controller.ResetY();
-            }
         }
 
         public override void Exit()
         {
             base.Exit();
-            _player.Input.PlayerInput.Jump.started -= JumpOnStarted;
-            _player.Input.PlayerInput.Hover.started -= HoverOnStarted;
+            _player.FallParticles.Play();
             _player.Controller.SetAnimation(PlayerAnimations.Base);
-        }
-
-        private void JumpOnStarted(InputAction.CallbackContext obj)
-        {
-            if (_player.CanDoubleJump && _player.HaveDoubleJump)
-            {
-                _stateMachine.ChangeState(_player.States.DoubleJumpState);
-            }
-            else if(_player.HaveHover)
-            {
-                _stateMachine.ChangeState(_player.States.HoveringState);
-            }
-        }
-
-        private void HoverOnStarted(InputAction.CallbackContext obj)
-        {
-            if (_player.HaveHover)
-            {
-                _stateMachine.ChangeState(_player.States.HoveringState);
-            }
         }
     }
 }
