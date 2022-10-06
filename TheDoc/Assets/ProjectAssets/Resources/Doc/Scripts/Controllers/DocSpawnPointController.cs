fdc2bf8e27cf000f5d026d64de09de95b4f3509f
StasVersh <71280619+StasVersh@users.noreply.github.com>
2022-10-07 00:23:28 +0300

    Create new instruments

--- TheDoc/Assets/ProjectAssets/Resources/Doc/Scripts/Controllers/DocSpawnPointController.cs
+++ TheDoc/Assets/ProjectAssets/Resources/Doc/Scripts/Controllers/DocSpawnPointController.cs
@@ -9,17 +9,17 @@ namespace ProjectAssets.Resources.Doc.Scripts.Controllers
     {
         [Inject] private DiContainer _container;
         [Inject] private Player _player;
-        [SerializeField] private List<GameObject> _spawnPoints;
+        [SerializeField] public List<GameObject> SpawnPoints;
         private void OnEnable()
         { 
             var player = _container.InstantiatePrefab(_player.Prefab);
             _player.GameObject = player;
             var index = _player.SpawnPointIndex;
-            if (index > (_spawnPoints.Count - 1))
+            if (index > (SpawnPoints.Count - 1))
             {
                 index = 0;
             }
-            _player.GameObject.transform.position = _spawnPoints[index].transform.position;
+            _player.GameObject.transform.position = SpawnPoints[index].transform.position;
         }
     }
 }
