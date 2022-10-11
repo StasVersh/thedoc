c09f93918cfcc2fcb089321c3fdc1cbf26698486
StasVersh <71280619+StasVersh@users.noreply.github.com>
2022-10-11 08:39:33 +0300

    Complite level design

---
+++ TheDoc/Assets/ProjectAssets/Resources/Scripts/Models/LayerColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectAssets.Resources.Scripts.Models
+{
+    [Serializable]
+    public class LayerColor
+    {
+        public string LayerName;
+        public Color Color;
+
+        public LayerColor(string layerName)
+        {
+            LayerName = layerName;
+            Color = Color.white;
+        }
+    }
+}
